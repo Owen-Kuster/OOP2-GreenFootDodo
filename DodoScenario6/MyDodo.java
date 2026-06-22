67f6019176e2d886f9bce8481943d05b1b665d6c
Owen Kuster <owenkuster08@gmail.com>
2026-06-22 13:21:36 +0200

    added methods
    added: SurpriseEgg getMostValuableEgg
    added: printCoordinatesAndValueOfEgg(Egg egg)
    added: testMostValuableEgg()
    added: printAverageValueOfEggs

--- DodoScenario6/MyDodo.java
+++ DodoScenario6/MyDodo.java
@@ -87,7 +87,7 @@ public class MyDodo extends Dodo
      */
     public void practiceWithLists( ){
         List<Integer> listOfNumbers = createListOfNumbers();
-        
+
         //the following is incorrect and is to be fixed in challenge 6.1c
         System.out.println("First element: " + listOfNumbers.get(1) ); 
     }
@@ -95,7 +95,7 @@ public class MyDodo extends Dodo
     public void practiceWithListsOfSurpriseEggs( ){
         List<SurpriseEgg>  listOfEgss = SurpriseEgg.generateListOfSurpriseEggs( 12, getWorld() );
     }
-    
+
     /**
      *  Test makeListOfSurpriseEggs
      *  
@@ -104,38 +104,98 @@ public class MyDodo extends Dodo
      *  <p> You can call it with getWorld()
      *  
      */
-    
+
     public void makeListOfSurpriseEggs(){
-         listOfEggs = SurpriseEgg.generateListOfSurpriseEggs( 10, getWorld() );
+        listOfEggs = SurpriseEgg.generateListOfSurpriseEggs( 10, getWorld() );
     }
-    
+
     /**
      * Test printCoordinatesOfEgg(Egg egg)
      * 
      * <p> Print coordinates of an egg
      * 
      */
-    
+
     public void printCoordinatesOfEgg(Egg egg){
         int eggX = egg.getX();
         int eggY = egg.getY();
-        
-        // listOfEggs
         System.out.println("( X = " + eggX + " , Y = " + eggY + ")");
     }
-    
+
     /**
      * Test makeListOfSurpriseEggsAndPrintCoordinates
      * 
      * <p> Use a foreach to print every coordinate from a surprise egg
      * 
      */
-    
+
     public void makeListOfSurpriseEggsAndPrintCoordinates(){
         makeListOfSurpriseEggs();
         for(Egg egg:listOfEggs){
             printCoordinatesOfEgg(egg);
         }
-        
+    }
+
+    /**
+     * Test getMostValuableEgg
+     * 
+     * <p> Initial: listOfEggs contains SurpriseEgg objects
+     * <p> Final: returns the egg with the highest value
+     * 
+     * <p> return SurpriseEgg with the highest value
+     * 
+     */
+    public SurpriseEgg getMostValuableEgg(){ //SurpriseEgg from the list
+        SurpriseEgg mostValuable = listOfEggs.get(0); //start with first egg
+        for (SurpriseEgg egg : listOfEggs){ //foreach of a for loop as the list above
+            if (egg.getValue() > mostValuable.getValue()){
+                mostValuable = egg;
+            }
+        }
+        return mostValuable; //return
+    }
+
+    /**
+     * Test printCoordinatesAndValueOfEgg
+     * 
+     * <p> Egg the egg to print info about
+     * 
+     */
+    public void printCoordinatesAndValueOfEgg(Egg egg){
+        int eggX = egg.getX();
+        int eggY = egg.getY();
+        int eggValue = egg.getValue();
+        System.out.println("( X = " + eggX + " , Y = " + eggY + " , Value = " + eggValue + " )");
+    }
+
+    /**
+     * Test testMostValuableEgg
+     * 
+     */
+    public void testMostValuableEgg(){
+        makeListOfSurpriseEggs(); //place 10 eggs in the world
+        System.out.println("All eggs:");
+        for (SurpriseEgg egg : listOfEggs){ //foreach of a for loop
+            printCoordinatesAndValueOfEgg(egg); //prints coordinates and value of the eggs
+        }
+        SurpriseEgg mostValuable = getMostValuableEgg(); //saving the most valuable egg in mostValuable van het type SurpriseEgg
+        System.out.println("Most valuable egg:");
+        printCoordinatesAndValueOfEgg(mostValuable);
+    }
+
+    /**
+     * Test printAverageValueOfEggs
+     * 
+     * <p> Initial: listOfEggs contains SurpriseEgg objects
+     * <p> Final: prints the average value of all eggs
+     */
+    public void printAverageValueOfEggs() {
+        makeListOfSurpriseEggs();
+        int total = 0;
+        for (SurpriseEgg egg : listOfEggs) { //foreach of a for loop
+            total = total + egg.getValue(); //total gets + egg value
+        }
+        double average = (double) total / listOfEggs.size(); //casting to double //divided by list size
+        System.out.println("Average Value: " + average); //print average
     }
 }
